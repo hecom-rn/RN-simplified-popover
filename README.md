0283d7d17afc8d8b80f548f3f73bb7aea8193b10
LJJ <lujunjian@hecom.cn>
2020-04-14 15:44:00 +0800

    补充readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# RN-simplified-popover
+# rn-simplified-popover
+[![npm version](https://img.shields.io/npm/v/@hecom/rn-simplified-popover.svg)](https://www.npmjs.com/package/@hecom/rn-simplified-popover)
+
 ReactNative气泡组件
+
+![examplePng](https://github.com/hecom-rn/rn-simplified-popover/tree/master/example/example.png)
--- example/App.js
+++ example/App.js
@@ -13,9 +13,9 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        isVisible: false
+      isVisible: false
     };
-}
+  }
 
   getInitialState() {
     return {
@@ -28,13 +28,13 @@ export default class extends React.Component {
     this.refs[ref].measure((ox, oy, width, height, px, py) => {
       this.setState({
         isVisible: true,
-        buttonRect: {x: px, y: py, width: width, height: height}
+        buttonRect: { x: px, y: py, width: width, height: height }
       });
     });
   }
 
   closePopover() {
-    this.setState({isVisible: false});
+    this.setState({ isVisible: false });
   }
 
   renderButton(text, ref) {
@@ -46,8 +46,8 @@ export default class extends React.Component {
   }
 
   render() {
-    var {width, height} = Dimensions.get('window');
-    var displayArea = {x: 5, y: 20, width: width - 10, height: height - 25};
+    var { width, height } = Dimensions.get('window');
+    var displayArea = { x: 5, y: 20, width: width - 10, height: height - 25 };
 
     return (
       <View style={styles.container}>
@@ -69,15 +69,15 @@ export default class extends React.Component {
           </View>
         </View>
         <Popover
-            isVisible={this.state.isVisible}
-            fromRect={this.state.buttonRect}
-            displayArea={displayArea}
-            placement = 'bottom'
-            onClose={this.closePopover.bind(this)}>
-            <View style={styles.popoverContent}>
-              <Text style={styles.popoverText}>Content</Text>
-            </View>
-          </Popover>
+          isVisible={this.state.isVisible}
+          fromRect={this.state.buttonRect}
+          displayArea={displayArea}
+          placement='bottom'
+          onClose={this.closePopover.bind(this)}>
+          <View style={styles.popoverContent}>
+            <Text style={styles.popoverText}>Content</Text>
+          </View>
+        </Popover>
       </View>
     );
   }
@@ -86,7 +86,7 @@ export default class extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(43, 186, 180)',
+    backgroundColor: '#5CACEE',
   },
   grid: {
     marginTop: 20,
